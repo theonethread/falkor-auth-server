7c5f8e07020af6f6b07efa519c7f23b46af14eab
Barnabas Bucsy <barnabas.bucsy@gmail.com>
2021-10-05 20:32:23 +0200

    Progress

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import cluster from "cluster";
 import path from "path";
 import shell from "shelljs";
 import logFactory from "pino";
+import minimist from "minimist";
 
 //#if _DEBUG
 import dotenv from "dotenv";
@@ -13,8 +14,58 @@ import configFactory from "./util/config.js";
 dotenv.config({ path: "res/config.env" });
 //#endif
 
+const argv = minimist(process.argv.slice(2));
+const retrieveOwnVersion = () =>
+    // NOTE: can't use __dirname in es module
+    JSON.parse(shell.cat(path.join(path.dirname(fileURLToPath(import.meta.url)), "../package.json"))).version;
+
+if (argv.v || argv.version) {
+    console.log("falkor-auth-server version", retrieveOwnVersion());
+    process.exit(0);
+}
+
+if (argv.h || argv.help) {
+    console.log(`
+[Falkor Authentication Server]
+version ${retrieveOwnVersion()}
+(C)2020-2021 Barnabas Bucsy - All rights reserved.
+
+Falkor Nginx authentication preflight proxy server - part of the Falkor Framework
+
+Usage:
+    falkor-auth-server (-v | --version | -h | --help)
+    falkor-auth-server
+
+Options:
+    -v, --version                   Show version and exit
+    -h, --help                      Show this screen and exit
+    -i <id>, --id <id>              ID of server
+    -p <port>, --port <port>        Port of server
+    -d <domain>, --domain <domain>  Domain of the cookies to set
+    -H <host>, --host <host>        Host of the server
+    -c <cookie>, --cookie <cookie>  Cookie name
+    -t <ttl>, --ttl <ttl>           Cookie TTL
+    -s <secret>, --secret <secret>  Server secret for toke encryption
+    -u <user>, --user <user>        User response header name
+    -r <role>, --role <role>        Role response header name
+    -D <db>, --db <db>              User database, either "mongodb+srv://" address, or relative path to yml file
+    -S <stamp>, --stamp <stamp>     Add timestamp to logs
+    -l <level>, --level <level>     Log level
+    -f <file>, --file <file>        Log file destination
+`);
+    process.exit(0);
+}
+
 // NOTE: must retrieve values after dotenv configured
-const config = configFactory();
+const config = configFactory(argv);
+
+if (!config.domain) {
+    throw "Error: Domain must be set (-d, --domain, SERVER_DOMAIN)";
+}
+
+if (!config.authDb) {
+    throw "Error: Mongodb address, or yaml file must be set (-D, --db, AUTH_DB)";
+}
 
 let destination = undefined;
 if (config.logFile) {
--- src/util/config.js
+++ src/util/config.js
@@ -3,26 +3,26 @@ import time from "./time.js";
 const trueValues = [true, 1, "1", "true", "yes"];
 const falseValues = [false, 0, "0", "false", "no"];
 
-export default () => {
+export default (argv) => {
     return {
         pid: process.pid,
         startTime: time.sec36(),
 
-        id: process.env.SERVER_ID || "falkor-auth",
-        port: +process.env.SERVER_PORT || 9999,
-        domain: process.env.SERVER_DOMAIN,
-        host: process.env.SERVER_HOST || "0.0.0.0",
+        id: argv.i || argv.id || process.env.SERVER_ID || "falkor-auth",
+        port: argv.p || argv.port || +process.env.SERVER_PORT || 9999,
+        domain: argv.d || argv.domain || process.env.SERVER_DOMAIN,
+        host: argv.H || argv.host || process.env.SERVER_HOST || "0.0.0.0",
 
-        cookieName: process.env.COOKIE_NAME || "@falkor_cloud_token",
-        cookieTtl: +process.env.COOKIE_TTL || 14400, // sec
+        cookieName: argv.c || argv.cookie || process.env.COOKIE_NAME || "@falkor_cloud_token",
+        cookieTtl: argv.t || argv.ttl || +process.env.COOKIE_TTL || 14400, // sec
 
-        authSecret: process.env.AUTH_SECRET || "%-super-secret-%",
-        authHeaderUser: process.env.AUTH_HEADER_USER || "X-Falkor-User",
-        authHeaderRole: process.env.AUTH_HEADER_ROLE || "X-Falkor-Role",
-        authDb: process.env.AUTH_DB,
+        authSecret: argv.s || argv.secret || process.env.AUTH_SECRET || "%-super-secret-%",
+        authHeaderUser: argv.u || argv.user || process.env.AUTH_HEADER_USER || "X-Falkor-User",
+        authHeaderRole: argv.r || argv.role || process.env.AUTH_HEADER_ROLE || "X-Falkor-Role",
+        authDb: argv.D || argv.db || process.env.AUTH_DB,
 
-        logTimestamp: !falseValues.includes(process.env.LOG_TIMESTAMP),
-        logLevel: process.env.LOG_LEVEL || "debug",
-        logFile: process.env.LOG_FILE || null
+        logTimestamp: argv.S || argv.stamp || !falseValues.includes(process.env.LOG_TIMESTAMP),
+        logLevel: argv.l || argv.level || process.env.LOG_LEVEL || "debug",
+        logFile: argv.f || argv.file || process.env.LOG_FILE || null
     };
 };
